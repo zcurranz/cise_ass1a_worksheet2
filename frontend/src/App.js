6ff6e4e3ab47b8a17acdd46f46cfdf19d1c921ba
Zachary Currran <zcurranz@gmail.com>
2021-07-26 15:49:30 +1200

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+const App = () => 
+<div className="App">
+    <h1>Welcome to CISE - the home of learning and fun</h1>
+    </div>
+
+export default App;
