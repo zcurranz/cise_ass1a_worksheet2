984b57d2f079aa81419a515786d8e4eed649afe4
Zachary Currran <zcurranz@gmail.com>
2021-07-26 15:56:05 +1200

    Changed the App.js file to have a count funtion

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+    }
+  }
 
-const App = () => 
-<div className="App">
-    <h1>Welcome to CISE - the home of learning and fun</h1>
-    </div>
+  makeIncrementer = amount => () =>
+    this.setState(prevState => ({
+      count: prevState.count + amount,
+    }));
+
+  increment = this.makeIncrementer(1);
+
+  render() {
+    return (
+      <div>
+        <p>Count: {this.state.count}</p>
+        <button className="increment" onClick={this.increment}>Increment count</button>
+      </div>
+    )
+  }
+}
 
 export default App;
+
+
